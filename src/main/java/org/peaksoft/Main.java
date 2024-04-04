dcf7dd82a597a37b12d377ba3a251f40b88a39fc
meder <meder.tynychbekov@gmail.com>
2024-04-04 20:38:55 +0600

    Changed Main class

--- src/main/java/org/peaksoft/Main.java
+++ src/main/java/org/peaksoft/Main.java
@@ -1,13 +1,10 @@
 package org.peaksoft;
 
-import org.peaksoft.service.CarServiceImpl;
-
 public class Main {
     public static void main(String[] args) {
         // TODO: 27.09.2023   реализуйте алгоритм здесь
 
-        CarServiceImpl carService = new CarServiceImpl();
-        carService.createTable();
+        System.out.println("hello world");
 
     }
 }
